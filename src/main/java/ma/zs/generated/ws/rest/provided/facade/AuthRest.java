f4d7216224e42d383a5d5201ff609e863c5d4127
Mohammed BENMANSOUR <med.benmansour1997@gmail.com>
2021-01-26 15:26:18 +0100

    --Register returns token+userVo

--- src/main/java/ma/zs/generated/ws/rest/provided/facade/AuthRest.java
+++ src/main/java/ma/zs/generated/ws/rest/provided/facade/AuthRest.java
@@ -1,11 +1,13 @@
 package ma.zs.generated.ws.rest.provided.facade;
 
 import io.swagger.annotations.Api;
+import ma.zs.generated.bean.User;
 import ma.zs.generated.service.facade.UserService;
 import ma.zs.generated.service.util.JwtUtil;
 import ma.zs.generated.ws.rest.provided.converter.UserConverter;
 import ma.zs.generated.ws.rest.provided.vo.AuthRequest;
 import ma.zs.generated.ws.rest.provided.vo.AuthResponse;
+import ma.zs.generated.ws.rest.provided.vo.UserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -30,25 +32,24 @@ public class AuthRest {
 
 
     @PostMapping(value = "/login")
-    public AuthResponse login(@RequestBody AuthRequest authRequest) throws Exception{
-		try {
+    public AuthResponse login(@RequestBody AuthRequest authRequest) throws Exception {
+        try {
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(
                             authRequest.getEmail(),
                             authRequest.getPassword()
                     )
             );
-        }catch (Exception e){
-		    throw new Exception("invalid credentials");
+        } catch (Exception e) {
+            throw new Exception("invalid credentials");
         }
-		userConverter.setRole(Boolean.TRUE);
-		userConverter.setCity(Boolean.TRUE);
-		userConverter.setSpecialityCollaborators(Boolean.TRUE);
-		userConverter.setRatings(Boolean.TRUE);
-		userConverter.setCommands(Boolean.TRUE);
+        userConverter.setRole(Boolean.TRUE);
+        userConverter.setCity(Boolean.TRUE);
+        userConverter.setSpecialityCollaborators(Boolean.TRUE);
+        userConverter.setRatings(Boolean.TRUE);
+        userConverter.setCommands(Boolean.TRUE);
 
-		return new AuthResponse(jwtUtil.generateToken(authRequest.getEmail()), userConverter.toVo(userService.findByEmail(authRequest.getEmail())));
-
+        return new AuthResponse(jwtUtil.generateToken(authRequest.getEmail()), userConverter.toVo(userService.findByEmail(authRequest.getEmail())));
 
 
         //		Object result = userService.login(authRequest);
@@ -60,6 +61,18 @@ public class AuthRest {
 //		data.put("message", "Logged in successfully");
 //		data.put("user", result);
 //		return ResponseEntity.status(HttpStatus.OK).body(userConverter.toVo((User) result));
+    }
+
+    @PostMapping(value = "/register")
+    public AuthResponse register(@RequestBody UserVo userVo) throws Exception {
+        userConverter.setRole(Boolean.TRUE);
+        userConverter.setCity(Boolean.TRUE);
+        userConverter.setSpecialityCollaborators(Boolean.TRUE);
+        userConverter.setRatings(Boolean.TRUE);
+        userConverter.setCommands(Boolean.TRUE);
+        User user = userConverter.toItem(userVo);
+        user = userService.save(user);
+        return new AuthResponse(jwtUtil.generateToken(userVo.getEmail()), userConverter.toVo(user));
     }
 
 
